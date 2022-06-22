bddfa595b12075dda4117b76416c9b6c4b785fdb
Tavor <tavorperry@gmail.com>
2022-06-22 14:05:11 +0700

    esLint style

--- txtFilesHandler.js
+++ txtFilesHandler.js
@@ -1,23 +1,23 @@
-import {promises as fsPromises} from "fs";
+import {promises as fsPromises} from 'fs';
 
 const asyncReadFile = async (filename, log) => {
-    try {
-        const contents = await fsPromises.readFile(filename, 'utf-8');
-        return contents.split(/\r?\n/);
-    } catch (err) {
-        log.error(err);
-    }
-}
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+    return contents.split(/\r?\n/);
+  } catch (err) {
+    log.error(err);
+  }
+};
 
 const writeToTxtFile = (path, content, log) => {
-    fsPromises.writeFile(path, '\n' + content, { flag: 'a+' }, err => {
-        if (err) {
-            log.error(err);
-        }
-    });
+  fsPromises.writeFile(path, '\n' + content, {flag: 'a+'}, (err) => {
+    if (err) {
+      log.error(err);
+    }
+  });
 };
 
 export {
-    asyncReadFile,
-    writeToTxtFile
-}
+  asyncReadFile,
+  writeToTxtFile,
+};
